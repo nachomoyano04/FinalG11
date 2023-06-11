246026c78aa4c6cf7a5603e3ad23196c504bf651
ismaeln23 <ismaelneira23@gmail.com>
2023-06-10 21:46:31 -0300

    MiembrosEquipo

--- src/controladoras/EquipoData.java
+++ src/controladoras/EquipoData.java
@@ -1,5 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
+/* 
+* To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -14,66 +14,68 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import javax.swing.JOptionPane;
+
 /**
  *
  * @author nacho
  */
 public class EquipoData {
+
     private Connection con = null;
-    
-    public EquipoData(){
+
+    public EquipoData() {
         con = Conexion.getConexion();
     }
-    
-    public void crearEquipo(Equipo equipo){
+
+    public void crearEquipo(Equipo equipo) {
         String sql = "INSERT INTO equipo (idProyecto, nombre, fechaCreacion, estado) VALUES (?,?,?,?)";
         PreparedStatement ps = null;
-        try{
-            ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1,equipo.getProyecto().getIdProyecto());
+        try {
+            ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, equipo.getProyecto().getIdProyecto());
             ps.setString(2, equipo.getNombre());
             ps.setDate(3, Date.valueOf(equipo.getFechaCreacion()));
-            ps.setBoolean(4,equipo.isEstado());
+            ps.setBoolean(4, equipo.isEstado());
             int exito = ps.executeUpdate();
             ResultSet res = ps.getGeneratedKeys();
-            if(exito == 1){
-                if(res.next()){
+            if (exito == 1) {
+                if (res.next()) {
                     equipo.setIdEquipo(res.getInt("idEquipo"));
                 }
                 JOptionPane.showMessageDialog(null, "Equipo añadido con éxito");
             }
             ps.close();
-        }catch(SQLException ex){
-            JOptionPane.showMessageDialog(null, "Error al crear equipo"+ex.getMessage());
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al crear equipo" + ex.getMessage());
         }
     }
 
-    public void darDeBajaEquipo(Equipo equipo){
+    public void darDeBajaEquipo(Equipo equipo) {
         String sql = "UPDATE equipo SET estado = 0 WHERE idEquipo = ?";
         PreparedStatement ps = null;
-        try{
+        try {
             ps = con.prepareStatement(sql);
             ps.setInt(1, equipo.getIdEquipo());
             int exito = ps.executeUpdate();
-            if(exito == 1){
-                JOptionPane.showMessageDialog(null, equipo.getNombre()+" dado de baja.");
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, equipo.getNombre() + " dado de baja.");
             }
             ps.close();
-        }catch(SQLException ex){
-            JOptionPane.showMessageDialog(null,"Error al dar de baja al equipo "+ex.getMessage());
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al dar de baja al equipo " + ex.getMessage());
         }
     }
-    
-    public Equipo buscarEquipoPorId(Equipo equipo){
+
+    public Equipo buscarEquipoPorId(Equipo equipo) {
         String sql = "SELECT equipo.nombre, equipo.fechaCreacion, equipo.estado FROM equipo WHERE idEquipo = ?";
         Equipo team = new Equipo();
         ProyectoData pd = new ProyectoData();
         PreparedStatement ps = null;
-        try{
+        try {
             ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setInt(1, equipo.getIdEquipo());
             ResultSet res = ps.executeQuery();
-            if(res.next()){
+            if (res.next()) {
                 team.setIdEquipo(equipo.getIdEquipo());
                 team.setProyecto(pd.buscarProyecto(res.getInt("idProyecto")));
                 team.setNombre(res.getString("nombre"));
@@ -81,8 +83,8 @@ public class EquipoData {
                 team.setEstado(res.getBoolean("estado"));
             }
             ps.close();
-        }catch(SQLException ex){
-            JOptionPane.showMessageDialog(null, "Error al buscar equipo "+ex.getMessage());
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al buscar equipo " + ex.getMessage());
         }
         return team;
     }
---
+++ src/controladoras/MiembrosEquipoData.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladoras;
+
+import entidades.Equipo;
+import entidades.Miembro;
+import entidades.MiembrosEquipo;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Isma
+ */
+public class MiembrosEquipoData {
+
+    private Connection con = null;
+
+    public MiembrosEquipoData() {
+        con = Conexion.getConexion();
+    }
+
+    public void crearMiembrosEquipo(MiembrosEquipo miembrosEquipo) {
+        String sql = "INSERT INTO miembros_equipo (idMiembro, idEquipo, fechaIncorporacion) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, miembrosEquipo.getMiembro().getIdMiembro());
+            ps.setInt(2, miembrosEquipo.getEquipo().getIdEquipo());
+            ps.setDate(3, Date.valueOf(miembrosEquipo.getFechaIncorporacion()));
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                miembrosEquipo.setIdMiembroEq(rs.getInt("idMiembroEq"));
+                JOptionPane.showMessageDialog(null, "Miembro de equipo creado con éxito");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla MiembrosEquipo: " + ex.getMessage());
+        }
+    }
+
+    public MiembrosEquipo buscarMiembrosEquipoPorId(int idMiembroEq) {
+        String sql = "SELECT * FROM miembros_equipo WHERE idMiembroEq = ?";
+        MiembrosEquipo miembrosEquipo = null;
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idMiembroEq);
+            ResultSet res = ps.executeQuery();
+            if (res.next()) {
+                miembrosEquipo = new MiembrosEquipo();
+                miembrosEquipo.setIdMiembroEq(res.getInt("idMiembroEq"));
+                miembrosEquipo.setMiembro(buscarMiembroPorId(res.getInt("idMiembro")));
+                miembrosEquipo.setEquipo(buscarEquipoPorId(res.getInt("idEquipo")));
+                miembrosEquipo.setFechaIncorporacion(res.getDate("fechaIncorporacion").toLocalDate());
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al buscar MiembrosEquipo: " + ex.getMessage());
+        }
+        return miembrosEquipo;
+    }
+
+    private Miembro buscarMiembroPorId(int idMiembro) {
+        MiembroData miembroData = new MiembroData();
+        return miembroData.buscarMiembroPorId(idMiembro);
+    }
+
+    private Equipo buscarEquipoPorId(int idEquipo) {
+        EquipoData equipoData = new EquipoData();
+        Equipo equipo = new Equipo();
+        equipo.setIdEquipo(idEquipo);
+        return equipoData.buscarEquipoPorId(equipo);
+    }
+
+}
